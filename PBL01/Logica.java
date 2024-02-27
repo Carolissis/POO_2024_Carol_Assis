90523afd0957a3fe5b95b4b73fa908e3a62855b9
Caroline Assis <caroolassissss@gmail.com>
2024-02-27 11:20:43 -0300

    calculadora

---
+++ PBL01/Logica.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Logica {
+    public Double Soma (Double x, Double y) {
+        return x + y;
+    }
+    public Double Subtracao (Double x, Double y) {
+        return x - y;
+    }
+    public Double Multiplicacao (Double x, Double y) {
+        return x * y;
+    }
+    public Double Divisao (Double x, Double y) {
+        return x / y;
+    }
+}

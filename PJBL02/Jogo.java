2343f120bf73dadcf4ffeb00e04cd0596f59e5f3
Caroline Assis <caroolassissss@gmail.com>
2024-03-04 12:38:46 -0300

    jogo completo

--- PJBL02/Jogo.java
+++ PJBL02/Jogo.java
@@ -7,11 +7,13 @@ public class Jogo {
 
         String[] jogadores = new String[5];
         int[] pontuacoes = new int[5];
-        int[] moedas = new int[5];
+        int[] moedasNv1 = new int[5];
+        int[] moedasNv2 = new int[5];
+        int[] moedasNv3 = new int[5];
         int qnt = 0;
 
         while(true) {
-            System.out.println("1) Adicionar um novo jogador com seu nome, pontuação inicial e as quantidades (max 5)");
+            System.out.println("1) Adicionar um novo jogador com seu nome, pontuação inicial e as quantidades de moedas coletadas em cada nível.(max 5)");
             System.out.println("2) Atualizar a pontuação de um jogador após a conclusão de um nível");
             System.out.println("3) Mostrar a pontuação total de um jogador");
             System.out.println("4) Mostrar o total de moedas coletadas em um determinado nível por todos os jogadores");
@@ -33,14 +35,20 @@ public class Jogo {
     
                             System.out.printf("Nome do jogador: ");
                             String jogador = ler.nextLine();
-                            System.out.printf("Pontuação: ");
+                            System.out.printf("Pontuação inicial: ");
                             int pontos = ler.nextInt();
-                            System.out.printf("Quantidade de moedas: ");
-                            int moeda = ler.nextInt();
+                            System.out.printf("Quantidade de moedas nivel 1: ");
+                            int moeda1 = ler.nextInt();
+                            System.out.printf("Quantidade de moedas nivel 2: ");
+                            int moeda2 = ler.nextInt();
+                            System.out.printf("Quantidade de moedas nivel 3: ");
+                            int moeda3 = ler.nextInt();
         
                             jogadores[qnt] = jogador;
                             pontuacoes[qnt] = pontos;
-                            moedas[qnt] = moeda;
+                            moedasNv1[qnt] = moeda1;
+                            moedasNv2[qnt] = moeda2;
+                            moedasNv3[qnt] = moeda3;
         
                             qnt += 1;
                         }
@@ -55,7 +63,7 @@ public class Jogo {
                             System.out.println("Quantos pontos? ");
                             int pontos = ler.nextInt();
                             pontuacoes[indice] += pontos;
-                            System.out.println(jogadores[indice] + " agora tem " + pontuacoes[indice+ " pontos");
+                            System.out.println(jogadores[indice] + " agora tem " + pontuacoes[indice]+ " pontos");
 
                         } else {
                             System.out.println("O nome " + jogador + " não foi encontrado no jogo.");
@@ -75,6 +83,35 @@ public class Jogo {
                         }
                         break;
                     case 4:
+                        System.out.printf("Qual nível:");
+                        int nivel = ler.nextInt();
+
+                        if(nivel == 1){
+
+                            for(int i = 1; i <= qnt; i ++){
+                                System.out.printf(jogador[i] + " coletou " + moedasNv1[i] " moedas");
+                            }
+                            break; 
+                            
+                        } else if(nivel == 2){
+
+                            for(int i = 1; i <= qnt; i ++){
+                                System.out.printf(jogador[i] + " coletou " + moedasNv2[i] " moedas");
+                            }
+                            break;
+
+                        } else if(nivel == 3){
+
+                            for(int i = 1; i <= qnt; i ++){
+                                System.out.printf(jogador[i] + " coletou " + moedasNv3[i] " moedas");
+                            }
+                            break;
+
+                        } else {
+                            System.out.println("Opção inválida!");
+                            break;
+                        }
+
                     case 5:
                         int maior = 0;
                         int index = -1;

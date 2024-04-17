d0edee61c319276400ed213fc7ad70c2fa09204a
Caroline Assis <caroolassissss@gmail.com>
2024-04-17 13:17:25 -0300

    ex formativa RA2 polimorfismo e class abstract

--- Formativas/FormativaRa2/ClassesAbstratas/Main.java
+++ Formativas/FormativaRa2/ClassesAbstratas/Main.java
@@ -7,3 +7,73 @@
 // classe pai.
 // 4. Instancie diferentes formas geométricas tridimensionais e chame seus
 // métodos para calcular área e perímetro
+
+public class Main{
+    public static void main(String[] args){
+
+        Esfera esf = new Esfera(5);
+        System.out.println("Area: " + esf.area());
+        System.out.println("Perimetro: " + esf.perimetro());
+
+        Cubo cub = new Cubo(3);
+        System.out.println("Area: " + cub.area());
+        System.out.println("Perimetro: " + cub.perimetro());
+
+        Cilindro cil = new Cilindro(2, 6);
+        System.out.println("Area: " + cil.area());
+        System.out.println("Perimetro: " + cil.perimetro());
+    }
+}
+
+abstract class FormaGeometrica{
+    private double x;
+
+    public FormaGeometrica(double x){
+        this.x = x;
+    }
+    public double getX(){
+        return this.x;
+    }
+    public abstract double area();
+    public abstract double perimetro();
+}
+
+class Esfera extends FormaGeometrica{
+    //x = raio
+    public Esfera(double raio){
+        super(raio);
+    }
+    public double area(){
+        return 4 * (3.14 * (getX() * getX()));
+    }
+    public double perimetro(){
+        return 2 * (3.14 * getX());
+    }
+}
+class Cubo extends FormaGeometrica{
+    //x = aresta
+    public Cubo(double aresta){
+        super(aresta);
+    }
+    public double area(){
+        return 6 * (getX() * getX());
+    }
+    public double perimetro(){
+        return 4 * getX();
+    }
+}
+class Cilindro extends FormaGeometrica{
+    //x = raio
+    private double altura; 
+
+    public Cilindro(double raio, double h){
+        super(raio);
+        this.altura = h;
+    }
+    public double area(){
+        return 2 * 3.14 * getX() * (getX() + this.altura);
+    }
+    public double perimetro(){
+        return (2 * 3.14 * getX()) + (2 * 3.14 * getX() * this.altura) ;
+    }
+}
--- Formativas/FormativaRa2/Polimorfismo/Main.java
+++ Formativas/FormativaRa2/Polimorfismo/Main.java
@@ -7,5 +7,85 @@
 // diferentes instâncias de animais.
 // 4. Explore o conceito de sobrescrita de métodos, alterando o
 // comportamento padrão de algum método em uma subclasse.
-// Classes Abstratas:
 
+public class Main{
+    public static void main(String[] args){
+        Animal animal = new Animal("Macaco");
+        animal.comer();
+        animal.comunicar();
+        Cachorro cachorro = new Cachorro("Tody", 5);
+        cachorro.comer();
+        cachorro.comunicar();
+        cachorro.exibirCachorro();
+        Gato gato = new Gato("Mia");
+        gato.comer();
+        gato.comunicar();
+        Passaro passaro = new Passaro("Molly");
+        passaro.comer();
+        passaro.comunicar();
+    }
+}
+
+class Animal{
+    private String nome;
+
+    public Animal(String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void comer(){
+        System.out.println("Animal come");
+    }
+    public void comunicar(){
+        System.out.println("Animal se comunica");
+    }
+}
+class Cachorro extends Animal{
+    private int idade;
+
+    public Cachorro(String nome, int idade){
+        super(nome);
+        this.idade = idade;
+    }
+    @Override
+    public void comer(){
+        System.out.println("Cachorro come");
+    }
+    @Override
+    public void comunicar(){
+        System.out.println("Au Au");
+    }
+    public void exibirCachorro(){
+        System.out.println(getNome() + " tem " + this.idade + " anos!");
+    }
+}
+class Gato extends Animal{
+
+    public Gato(String nome){
+        super(nome);
+    }
+    @Override
+    public void comer(){
+        System.out.println("Gato come");
+    }
+    @Override
+    public void comunicar(){
+        System.out.println("Miau");
+    }
+}
+class Passaro extends Animal{
+
+    public Passaro(String nome){
+        super(nome);
+    }
+    @Override
+    public void comer(){
+        System.out.println("Passaro come");
+    }
+    @Override
+    public void comunicar(){
+        System.out.println(getNome() + ": Piu Piu");
+    }
+}

dbff9432ff82381941beccd1470206d38a1c59ca
Caroline Assis <caroolassissss@gmail.com>
2024-04-17 14:57:13 -0300

    formativa rA2 interfaces e metodos abstratos

--- Formativas/FormativaRa2/Interfaces/Main.java
+++ Formativas/FormativaRa2/Interfaces/Main.java
@@ -7,3 +7,71 @@
 // 4. Crie um sistema de login que aceite qualquer objeto que implemente a
 // interface Autenticavel.
 // 5. Teste o sistema realizando login com diferentes tipos de usuários.
+
+public class Main {
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario("usuario123");
+        Administrador administrador = new Administrador("admin123");
+        Cliente cliente = new Cliente("cliente123");
+
+        SistemaLogin sistema = new SistemaLogin();
+
+        System.out.println("Testando login de usuário:");
+        sistema.login(usuario, "usuario123");  
+        System.out.println("Testando login de administrador:");
+        sistema.login(administrador, "senhaErrada");  
+        System.out.println("Testando login de cliente:");
+        sistema.login(cliente, "cliente123");  
+    }
+}
+interface Autenticavel {
+    boolean autenticar(String senha);
+}
+class Usuario implements Autenticavel {
+    private String senha;
+
+    public Usuario(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
+    }
+}
+
+class Administrador implements Autenticavel {
+    private String senha;
+
+    public Administrador(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
+    }
+}
+
+class Cliente implements Autenticavel {
+    private String senha;
+
+    public Cliente(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticar(String senha) {
+        return this.senha.equals(senha);
+    }
+}
+class SistemaLogin {
+    public void login(Autenticavel usuario, String senha) {
+        boolean autenticado = usuario.autenticar(senha);
+        if (autenticado) {
+            System.out.println("Login bem-sucedido!");
+        } else {
+            System.out.println("Falha no login!");
+        }
+    }
+}
--- Formativas/FormativaRa2/MetodosAbstratos/Main.java
+++ Formativas/FormativaRa2/MetodosAbstratos/Main.java
@@ -4,3 +4,68 @@
 // ContaCorrente e ContaPoupanca, e implemente os métodos abstratos.
 // 3. Teste as subclasses realizando operações de depósito, saque e
 // verificação de saldo.
+
+public class Main{
+    public static void main(String[] args){
+        ContaCorrente corrente = new ContaCorrente(600.00);
+        ContaPoupanca poupanca = new ContaPoupanca(200.00);
+        
+        System.out.println("Conta corrente saldo: R$" + corrente.getSaldo() + "\nSacando R$200.00, saldo atual: R$" + corrente.sacar(200.00));
+        System.out.println("Conta poupanca saldo: R$" + poupanca.getSaldo() + "\nDepositando R$200.00, saldo atual: R$" + poupanca.depositar(200.00));
+    }
+}
+
+abstract class Conta{
+    private double saldo;
+
+    public Conta(double saldo){
+        this.saldo = saldo;
+    }
+    public double getSaldo(){
+        return this.saldo;
+    }
+    protected void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public abstract double sacar(double valorSaque);
+    public abstract double depositar(double valorDeposito);
+}
+class ContaCorrente extends Conta{
+    public ContaCorrente(double saldo){
+        super(saldo);
+    }
+    public double sacar(double valorSaque){
+        if(valorSaque > getSaldo()){
+            System.out.println("Saque inválido!");
+            return getSaldo();
+        }
+        else{
+            setSaldo(getSaldo() - valorSaque);
+            return getSaldo();            
+        }
+    }
+    public double depositar(double valorDeposito){
+        setSaldo(getSaldo() + valorDeposito);
+        return getSaldo();
+    }
+}
+class ContaPoupanca extends Conta{
+    public ContaPoupanca(double saldo){
+        super(saldo);
+    }
+    public double sacar(double valorSaque){
+        if(valorSaque > getSaldo()){
+            System.out.println("Saque inválido!");
+            return getSaldo();
+        }
+        else{
+            setSaldo(getSaldo() - valorSaque);
+            return getSaldo();            
+        }
+    }
+    public double depositar(double valorDeposito){
+        setSaldo(getSaldo() + valorDeposito);
+        return getSaldo();
+    }
+}
